de34b3860a1920e23837caae22b55391fe648b51
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2023-12-22 16:17:21 +0530

    Updated

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Build Stage
 FROM maven:3.8.4-openjdk-17 AS build
 COPY pom.xml .
-RUN mvn clean package -U
+RUN mvn clean package
 
 # Runtime Stage
 FROM openjdk:17-jdk-slim
--- src/main/java/com/rjtechx/RjtecXDashApplication.java
+++ src/main/java/com/rjtechx/RjtecXDashApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RjtecXDashApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RjtecXDashApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RjtecXDashApplication.class, args);
+    }
 }
